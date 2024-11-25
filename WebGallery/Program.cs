07c5d565d664537f10ce41bb3ae8fbe63d9d79fb
Axel-Cleris Gailloty <agailloty@gmail.com>
2024-11-25 21:43:37 +0100

    Ajoutez des fichiers projet.

---
+++ WebGallery/Program.cs
@@ -0,0 +1,34 @@
+using WebGallery.Components;
+using WebGallery.Services;
+
+namespace WebGallery
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddRazorComponents()
+                .AddInteractiveServerComponents();
+            builder.Services.AddSingleton<ImageService>();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Error");
+            }
+
+            app.UseAntiforgery();
+
+            app.MapStaticAssets();
+            app.MapRazorComponents<App>()
+                .AddInteractiveServerRenderMode();
+
+            app.Run();
+        }
+    }
+}
---
+++ WebGallery/Services/ImageService.cs
@@ -0,0 +1,18 @@
+﻿using System.IO;
+
+namespace WebGallery.Services
+{
+    public class ImageService
+    {
+        public string[] GetImageFiles(string directoryPath)
+        {
+            if (Directory.Exists(directoryPath))
+            {
+                return Directory.GetFiles(directoryPath, "*.*", SearchOption.TopDirectoryOnly)
+                                .Where(file => file.EndsWith(".jpg") || file.EndsWith(".png") || file.EndsWith(".gif"))
+                                .ToArray();
+            }
+            return Enumerable.Empty<string>().ToArray();
+        }
+    }
+}
